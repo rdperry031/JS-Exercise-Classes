8b206ebcbab4c045f8629cdd60dfa3a8305fd2ff
rdperry031 <rdperry031@gmail.com>
2021-06-17 17:43:06 -0500

    completed task 4

--- index.js
+++ index.js
@@ -138,8 +138,20 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
-
+ class Instructor extends Lambdasian {
+   constructor(att){
+     super(att);
+     this.specialty = att.specialty;
+     this.favLanguage = att.favLanguage;
+     this.catchPhrase = att.catchPhrase;
+    
+    }
+    demo(subject){
+      return `Today we are learning about ${subject}`
+    }
+    grade(student, subject){
+      return `${student.name} receives a perfect score on ${subject}`
+    }
  }
   /*
     TASK 5
